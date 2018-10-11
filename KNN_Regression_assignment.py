20bb032c27fd2d7df1d554ab228eabb42709b7ee
Uday Keith <udaykeith@gmail.com>
2018-10-11 22:45:52 +0530

    Add files via upload

---
+++ KNN_Regression_assignment.py
@@ -0,0 +1,32 @@
+#!/usr/uday/anaconda/bin/python3
+
+"""*********************************************
+ Write a Class called KNN that implements the KNN Regression algorithm using 
+ the Scipy KDTree. This class should be able to do the following given training
+ and test data: 
+
+ 1) Calculate K-nearest neighbouts of given point
+ 2) Predict value using K-nearest points (taking average)
+
+
+
+
+*********************************************"""
+
+import pandas as pd 
+import numpy as np 
+from scipy.spatial import KDTree
+import random 
+import numpy.random as npr
+from sklearn.preprocessing import StandardScaler 
+import sys
+
+sys.setrecursionlimit(1000)
+
+class KNN_Regression:
+	def __init__():
+
+
+	def calculate_KNeigbours():
+
+	def predict()
